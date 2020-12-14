d9836dafab23b87e41c492a09cf554aae0451dd4
nemixu <s.r.seagrave@gmail.com>
2020-12-14 20:04:58 +0000

    added logic for searching the api and appending child to document.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,24 @@
-const apikey = "";
-const url = "";
-const q = document.getElementById("#q");
+const apiKey = config.apiKey;
+let input = document.getElementById("input").value;
+const img = document.createElement('img');
+const images = document.getElementById('image')
+let gifResults = [];
 
 
 const search = () => {
-  fetch(`http://api.giphy.com/v1/gifs/search?q=${q}&api_key=${apikey}&limit=5`)
-  .then(result => result.json())
-  .then(data => {
-    
-  })
+  let input = document.getElementById("input").value;
+  const url = `http://api.giphy.com/v1/gifs/search?q=${input}&api_key=${apiKey}&limit=50`;
+    fetch(url)
+      .then(result => result.json())
+      .then(data => {
+        let gifs = data.data;
+        console.log(gifs);
+        for(let i=0; i < gifs.length; i++ ){
+          let img = document.createElement("img");
+          let images = document.getElementById('image').appendChild(img);
+          images.setAttribute('src', gifs[i].images.original.url);
+          images.setAttribute('class', 'gif-images');
+        }
+      })
 }
-
-console.log("test")
-
+ 
